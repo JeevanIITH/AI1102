71e1a4f00d6eb5a3722f7384d0744370dff63e02
JeevanIITH <80705736+JeevanIITH@users.noreply.github.com>
2021-04-22 21:01:51 +0530

    Add files via upload

--- assignment3/cdf_a3.py
+++ assignment3/cdf_a3.py
@@ -1,16 +1,23 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import pyplot as plt
 import math
 
-x=np.linspace(-1,1,10000)
+def f(x):
+ if(-1<x<1): return (1/2)*(x+1)
+ if(x>1): return 1
+ else: return 0
 
-F_x= (1/2)*(x+1)
+x = np.linspace(-2,2,10000)
+
+y = []
+for i in range(len(x)):
+   y.append(f(x[i]))
 
 
 plt.xlabel("X")
 plt.ylabel("F(x)")
 plt.title("CDF")
 
-plt.plot(x,F_x)
-plt.show()
+plt.plot(x,y)
 
+plt.show()
